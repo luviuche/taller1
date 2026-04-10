db000da3be1b280c7599d1b7549626a63f1e6f34
lmvm <lmvm.zzz@gmail.com>
2026-04-10 13:24:52 -0500

    Crea el servicio de conversión para el ejercicio 2

---
+++ src/main/java/acm/talleres/taller1/ejercicio2/servicio/ServicioConversion.java
@@ -0,0 +1,23 @@
+package acm.talleres.taller1.ejercicio2.servicio;
+
+import acm.talleres.taller1.ejercicio2.interfaces.Conversor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServicioConversion {
+
+    private final Conversor conversor;
+
+    public ServicioConversion(@Qualifier("conversorTemperatura") Conversor conversor) {
+        this.conversor = conversor;
+    }
+
+    public void ejecutarProceso(double valorAConvertir) {
+        System.out.println("--- Iniciando proceso de conversion ---");
+        double resultado = conversor.convertir(valorAConvertir);
+        System.out.println("Valor original: " + valorAConvertir);
+        System.out.println("Resultado de la conversión: " + resultado);
+        System.out.println("---------------------------------------");
+    }
+}
